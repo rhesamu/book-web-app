25f325cc01e4521439a3fbb6acba226caef1e866
rhesamu <rhesamu.dev@gmail.com>
2024-06-20 22:23:22 +0700

    feat: add browser router logic

--- src/main.tsx
+++ src/main.tsx
@@ -2,16 +2,32 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
-import App from './App.tsx'
+import { Books, CreateBook, EditBook } from '@/views';
 import './index.scss'
 
 const queryClient = new QueryClient();
 
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Books />,
+  },
+  {
+    path: '/create',
+    element: <CreateBook />
+  },
+  {
+    path: '/edit/:bookId',
+    element: <EditBook />
+  }
+]);
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <App />
+      <RouterProvider router={router} />
     </QueryClientProvider>
   </React.StrictMode>,
 )
